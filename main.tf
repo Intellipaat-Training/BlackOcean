01e2549a30d998dae33148d5ccd33cf23e82fa24
unknown <imperfectlyperfect3jan@gmail.com>
2024-04-27 18:47:55 +0530

    added Key

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "k8-M" {
   ami = "ami-0da7657fe73215c0c"
   instance_type = "t2.medium"
-  key_name = "Aksith-Don't delete"
+  key_name = "AksithApril24"
   tags = {
     Name = "Kmaster"
   }
@@ -15,7 +15,7 @@ resource "aws_instance" "k8-M" {
 resource "aws_instance" "k8-S1" {
   ami = "ami-0da7657fe73215c0c"
   instance_type = "t2.medium"
-  key_name = "Aksith-Don't delete"
+  key_name = "AksithApril24"
   tags = {
     Name = "Kslave"
   }
